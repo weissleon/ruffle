9ff9baa6c97935248f30d2a2dbaae4f3af1063c5
weissleon <weissleon1943@gmail.com>
2022-01-24 15:24:20 +0900

    Modify index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
+   <>
    <div className='bg-blue-400'>Hi Sojeong</div>
+   <div></div>
+   </>
   )
 }
+
+// function Home(){
+//   return (
+//     <div className='bg-blue-400'>Hi Sojeong</div>
+//   )
+// }
 
 export default Home
