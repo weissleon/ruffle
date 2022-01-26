01d32a59b8fb7288a5c9e466f64dcd1fd960d6f3
YoonPasta <yoonpasta@gmail.com>
2022-01-26 14:41:18 +0900

    PASTA YOON

--- pages/result.tsx
+++ pages/result.tsx
@@ -11,20 +11,21 @@ const Result: NextPage<Props> = ({ winnerList }) => {
   return (
     <div className="min-h-screen flex flex-col gap-y-12 justify-center items-center bg-gradient-to-tr from-slate-500 to-slate-700">
       <div>
-        <h1 className="font-bold text-2xl">추첨 결과</h1>
-      </div>
-      <div className="justify-center flex items-center">
-        <div className="flex flex-row gap-x-4 justify-center items-center">
-          <div className="hover:bg-slate-100 active:bg-slate-200 cursor-pointer w-12 h-12 justify-center items-center flex rounded-full text-lg">
-            <IoChevronBack />
-          </div>
-          <main className="flex flex-row gap-x-2 flex-wrap max-w-[1072px] gap-y-2">
-            {winnerList.map((content) => (
-              <Card content={content} />
-            ))}
-          </main>
-          <div className="hover:bg-slate-100 active:bg-slate-200 cursor-pointer w-12 h-12 justify-center items-center flex rounded-full text-lg">
-            <IoChevronForward />
+        {/* 큰박스 */}
+        <div className="justify-center flex h-screen items-center">
+          {/* result box */}
+          <div className="flex-col">
+            <h1 className="flex-col w-full bg-green-700 text-center">
+              추첨 결과
+            </h1>
+            {/* result */}
+            <div className="w-full bg-yellow-500 text-center">
+              {winnerList.map((item, index) => (
+                <div className="w-48" key={index}>
+                  {item}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
