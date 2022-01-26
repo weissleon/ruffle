95d3121c3f72fad47aa0f3c21f523a0fec8535c9
weissleon <weissleon1943@gmail.com>
2022-01-26 11:56:42 +0900

    Modify design

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,37 +1,45 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
+import { generateWinnerList, ItemList } from "../lib/helper";
 
 type Props = {
   winnerList: string[];
 };
 const Result: NextPage<Props> = ({ winnerList }) => {
   return (
-    <>
-      <div>
-        <h1>추첨 결과</h1>
-        <div>
+    <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-tr from-slate-500 to-slate-700">
+      <div className="flex flex-col gap-y-4 items-center w-2/3 max-w-md bg-slate-100 px-4 py-4">
+        <h1 className="font-bold">추첨 결과</h1>
+        <div className="bg-white h-52 flex flex-col w-full py-2 gap-y-2 flex-wrap overflow-y-auto">
           {winnerList.map((item, index) => (
-            <div key={index}>{item}</div>
+            <div className="px-4" key={index}>
+              {item}
+            </div>
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const itemList = query.itemList as string[];
-  const winnerSet: Set<string> = new Set();
-  const PICK_NUM = Math.ceil(itemList.length / 2);
+  // Convert query.itemList to string [] no matter what the incoming type is.
 
-  while (winnerSet.size < PICK_NUM) {
-    const index = Math.floor(Math.random() * itemList!.length);
-    winnerSet.add(itemList[index]);
-  }
+  const itemList: ItemList =
+    query.itemList && typeof query.itemList === "string"
+      ? JSON.parse(query.itemList)
+      : {};
 
+  // ! TEMPORARY: Generating pickCount. In the future, the pickCount will be received as a query parameter.
+  const pickCount = Math.ceil(Object.keys(itemList).length / 2);
+
+  // Generate winnerList
+  const winnerList = generateWinnerList(itemList, pickCount);
+
+  // Pass winnerList to the client.
   return {
     props: {
-      winnerList: Array.from(winnerSet),
+      winnerList,
     },
   };
 };
